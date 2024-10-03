28a99bb73b7cd6e594e8147722d4df306bad3887
raghavrathi10 <33197780+raghavrathi10@users.noreply.github.com>
2024-10-02 20:47:42 -0400

    Delete stana_code directory

--- stana_code/stana_check_neg_freq.m
+++
@@ -1,6 +0,0 @@
-function res = stana_check_neg_freq(a, b)
-    if a > b/2
-        res = a - b;
-    else
-        res = a;
-    end
--- stana_code/updateTLE.m
+++
@@ -1,5 +0,0 @@
-command = 'curl -o All_sat.tle  https://celestrak.org/NORAD/elements/supplemental/sup-gp.php?FILE=starlink&FORMAT=tle';
-status = system(command);
-if status==0
-    fprintf("TLE updated successfully\n");
-end
